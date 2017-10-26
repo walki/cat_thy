28cec1ad27b1b9f9d0114a8431cfa2af9c752707
Roger Walker <roger.john.walker@gmail.com>
2017-10-25 23:25:15 -0400

    adding category theory scratchpad

---
+++ cat_thy/Composition.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat_thy
+{
+	public static class Composition
+	{
+		public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> f, Func<T2, T3> g)
+		{
+			return x => g(f(x));
+		}
+
+		public static Func<T1, T2> FunctionId<T1, T2>(Func<T1, T2> f)
+		{
+			return f;
+		}
+
+
+		public static T Id<T>(T t)
+		{
+			return t;
+		}
+
+		public static double DoubleId(double d)
+		{
+			return d;
+		}
+
+		public static void Test()
+		{
+			Func<Func<int, int>, Func<int, int>> identityInt = delegate (Func<int, int> f)
+			{
+				return f;
+			};
+
+			
+
+			var incr = identityInt(AddOne);
+			Func<int,int> incr2 = FunctionId<int,int>(AddOne);
+			
+
+			System.Console.WriteLine(incr(99));
+			System.Console.WriteLine(incr2(999));
+
+
+			var addThenDouble = Compose<int, int, double>(AddOne, Twice);
+			Console.WriteLine(addThenDouble(12));
+
+			var sqrtThenPrint = Compose<int, double, string>(SquareRoot, PrintResult);
+			Console.WriteLine(sqrtThenPrint(122121));
+
+
+		}
+
+		public static double SquareRoot(int x)
+		{
+			return Math.Sqrt(x);
+		}
+
+		public static int AddOne(int x)
+		{
+			return x + 1;
+		}
+
+		public static double Twice(int x)
+		{
+			return x * 2.0;
+		}
+
+		public static string PrintResult(double x)
+		{
+			return String.Format("The result was {0:0.0000}", x);
+		}
+
+	}
+}
---
+++ cat_thy_tests/CompositionTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cat_thy.tests
+{
+	[TestClass]
+	public class CompositionTests
+	{
+		[TestMethod]
+		public void IdTest()
+		{
+			// Arrange
+			// Setting up a function to pass to Id
+			Func<int, int> triple = delegate (int x) { return x * 3; };
+			
+
+			// Act
+			// Pass the function to Id
+			Func<int, int> idTriple = Composition.FunctionId<int, int>(triple);
+
+			// Assert
+			Assert.AreEqual(triple, idTriple);
+			
+		}
+
+
+		
+		[TestMethod]
+		public void ComposeTest()
+		{
+			// Arrange
+			// Set up two functions
+			Func<double, double> f = Math.Sqrt;
+			Func<double, double> g = Math.Log10;
+
+			// Act
+			// Compose the f o g
+			Func<double, double> FoG = Composition.Compose<double, double, double>(f, g);
+
+			double test = 100.001;
+			double sq = Math.Sqrt(test);
+			double lg = Math.Log10(sq);
+			Assert.AreEqual(lg, FoG(100.001));
+
+		}
+
+		[TestMethod]
+		public void ComposeIdTest()
+		{
+			//Write a program that tries to test that your composition function	respects identity.
+
+			// Arrange
+			Func<double, double> f = Math.Sqrt;
+			//Func<double, double> Id_f = Composition.DoubleId;
+			Func<double, double> Id_f = Composition.Id;
+
+			// Act
+			Func<double, double> FoId_f = Composition.Compose<double, double, double>(f, Id_f);
+
+			// Assert
+			double test = 100.001;
+			double sq = f(test);
+			Assert.AreEqual(sq, FoId_f(test));
+
+
+		}
+
+	}
+}
